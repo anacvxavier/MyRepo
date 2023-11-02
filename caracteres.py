04d6d4e606ed10707d0367c438947b24c29a1a96
anacvxavier <anac.vxavier@gmail.com>
2023-11-02 09:45:42 -0300

    string em python parte 2

---
+++ caracteres.py
@@ -0,0 +1,9 @@
+multiline = "Facts about the Moon:\n There is no atmosphere.\n There is no sound."
+print(multiline)
+
+print("")
+
+multiline = """Facts about the Moon:
+ There is no atmosphere. 
+ There is no sound."""
+print(multiline)
