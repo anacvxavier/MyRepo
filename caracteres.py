95d9db472803409db6c28da0e8efa74c6e50aa87
anacvxavier <anac.vxavier@gmail.com>
2023-11-02 11:41:04 -0300

    math

--- caracteres.py
+++ caracteres.py
@@ -1,3 +1,4 @@
+'''
 multiline = "Facts about the Moon:\n There is no atmosphere.\n There is no sound."
 print(multiline)
 
@@ -7,3 +8,9 @@ multiline = """Facts about the Moon:
  There is no atmosphere. 
  There is no sound."""
 print(multiline)
+'''
+
+# .title() coloca todas as primeuras letra em maiúsculo
+heading = "temperatures and facts about the moon"
+heading_upper = heading.title()
+print(heading_upper)
---
+++ contas.py
@@ -0,0 +1,7 @@
+seconds = 1042
+display_minutes = 1042 // 60
+#print(display_minutes)
+display_seconds = 1042 % 60
+
+print('')
+print(f"""{seconds} segundos correspondem a {display_minutes} minutos e {display_seconds} segundos""")
